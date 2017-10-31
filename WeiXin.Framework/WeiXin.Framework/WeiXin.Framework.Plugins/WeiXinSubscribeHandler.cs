6ca5f7dd5cba3a9907f905b1106d66661b74b0c2
wenjungg <847055719@qq.com>
2017-10-31 18:35:30 +0800

    ww

---
+++ WeiXin.Framework/WeiXin.Framework/WeiXin.Framework.Plugins/WeiXinSubscribeHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml.Linq;
+using WeiXin.Framework.Core;
+
+namespace WeiXin.Framework.Plugins
+{
+    /// <summary>
+    /// 微信用户关注事件控制器
+    /// </summary>
+    public class WeiXinSubscribeHandler : WeiXinHandlerType
+    {
+        #region 字段/属性
+
+        /// <summary>
+        /// 消息类型Event
+        /// </summary>
+        public override WeiXinMsgType WeiXinMsgType
+        {
+            get { return WeiXinMsgType.Event; }
+        }
+
+        /// <summary>
+        /// 用户关注事件
+        /// </summary>
+        public override WeiXinEventType WeiXinEventType
+        {
+            get { return WeiXinEventType.SubScribe; }
+        }
+
+        #endregion
+
+        public override void ProcessWeiXin(WeiXinContext context)
+        {
+            XElement result = new XElement("xml", new XElement("ToUserName", context.Request.FromUserName),
+                new XElement("FromUserName", context.Request.ToUserName),
+                new XElement("CreateTime", DateTime.Now.GetInt()),
+                new XElement("MsgType", WeiXinMsgType.Text.ToString().ToLower()),
+                new XElement("Content", "欢迎关注***的微信订阅号.回复试试,惊喜不断!微信机器人"));
+            context.Response.Write(result);
+        }
+    }
+}
---
+++ WeiXin.Framework/WeiXin.Framework/WeiXin.Framework.Web/Module/RequestLog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using WeiXin.Framework.Web.Helper;
+
+namespace WeiXin.Framework.Web.Module
+{
+    /// <summary>
+    /// 记录请求日志
+    /// </summary>
+    public class RequestLog : IHttpModule
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.EndRequest += Application_EndRequest;
+        }
+
+        /// <summary>
+        /// 添加请求日志
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_EndRequest(object sender, EventArgs e)
+        {
+            var context = HttpContext.Current;
+
+            string para = "";
+
+            foreach (var item in context.Request.Form.AllKeys)
+            {
+                para += item + ":" + context.Request.Form[item] + "; ";
+            }
+
+
+
+
+            string RequestUrl = HttpContext.Current.Request.Url.ToString();
+            string RequestParameter = para;
+
+
+            Stream stream = context.Request.InputStream;
+            StreamReader reader = new StreamReader(stream);
+            string text = reader.ReadToEnd();
+
+            LogHelper.WriteLog(RequestUrl + "----参数：" + para + "---- body：" + text + " type：" + context.Request.HttpMethod);
+        }
+    }
+}
